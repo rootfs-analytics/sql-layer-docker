3390aa964252ab76e4d3ef4d1e2352a4fbf50ecb
Mike McMahon <mmcm@comcast.net>
2015-01-21 17:00:47 -0500

    Tighten up running fdbmonitor.

--- fdb-server/docker-start.sh
+++ fdb-server/docker-start.sh
@@ -1,29 +1,30 @@
 #/bin/bash
 
-if [ ! -f /etc/foundationdb/foundationdb.conf ]; then
-    cp /usr/lib/foundationdb/foundationdb.conf.orig /etc/foundationdb/foundationdb.conf
-fi
-
 HOSTADDR=$(grep $HOSTNAME /etc/hosts | cut -f1)
 COORDADDR=$HOSTADDR:4500
+CONFFILE=/etc/foundationdb/$HOSTNAME.conf
 
-if [ -n "$FDB_PROCESS_COUNT" -o -n "$FDB_PUBLIC_ADDR" ]; then
-    if [ -z "$FDB_PROCESS_COUNT" ]; then FDB_PROCESS_COUNT=1; fi
-    if [ -n "$FDB_PUBLIC_ADDR" ]; then FDB_PUBLIC_PORTS=(${FDB_PUBLIC_PORT//,/ }); fi
+if [ ! -f $CONFFILE ]; then
+    cp /usr/lib/foundationdb/foundationdb.conf.orig $CONFFILE
 
-    sed -i -e 's/\[fdbserver\.4500\]//' /etc/foundationdb/foundationdb.conf
+    if [ -n "$FDB_PROCESS_COUNT" -o -n "$FDB_PUBLIC_ADDR" ]; then
+        if [ -z "$FDB_PROCESS_COUNT" ]; then FDB_PROCESS_COUNT=1; fi
+        if [ -n "$FDB_PUBLIC_ADDR" ]; then FDB_PUBLIC_PORTS=(${FDB_PUBLIC_PORT//,/ }); fi
 
-    N=0
-    while [ $N -lt $FDB_PROCESS_COUNT ]; do
-        echo >>/etc/foundationdb/foundationdb.conf
-        echo "[fdbserver.$((4500+N))"] >>/etc/foundationdb/foundationdb.conf
-        if [ -n "$FDB_PUBLIC_ADDR" ]; then
-            if [ $N -eq 0 ]; then COORDADDR=$FDB_PUBLIC_ADDR:${FDB_PUBLIC_PORTS[$N]}; fi
-            echo "listen_address = $HOSTADDR:$((4500+N))" >>/etc/foundationdb/foundationdb.conf
-            echo "public_address = $FDB_PUBLIC_ADDR:${FDB_PUBLIC_PORTS[$N]}"  >>/etc/foundationdb/foundationdb.conf
-        fi
-        N=$((N+1))
-    done
+        sed -i -e 's/\[fdbserver\.4500\]//' $CONFFILE
+
+        N=0
+        while [ $N -lt $FDB_PROCESS_COUNT ]; do
+            echo >>$CONFFILE
+            echo "[fdbserver.$((4500+N))"] >>$CONFFILE
+            if [ -n "$FDB_PUBLIC_ADDR" ]; then
+                if [ $N -eq 0 ]; then COORDADDR=$FDB_PUBLIC_ADDR:${FDB_PUBLIC_PORTS[$N]}; fi
+                echo "listen_address = $HOSTADDR:$((4500+N))" >>$CONFFILE
+                echo "public_address = $FDB_PUBLIC_ADDR:${FDB_PUBLIC_PORTS[$N]}"  >>$CONFFILE
+            fi
+            N=$((N+1))
+        done
+    fi
 fi
 
 if [ ! -f /etc/foundationdb/fdb.cluster ]; then
@@ -33,13 +34,16 @@ if [ ! -f /etc/foundationdb/fdb.cluster ]; then
     NEWDB=yes
 fi
 
-chmod go+rx /fdb-data
-DATADIR=/fdb-data/$HOSTNAME
-mkdir $DATADIR
-chown foundationdb:foundationdb $DATADIR
-ln -s $DATADIR /var/lib/foundationdb/data
+if [ ! -d /var/lib/foundationdb/data ]; then
+    chmod go+rx /fdb-data
+    DATADIR=/fdb-data/$HOSTNAME
+    mkdir $DATADIR
+    chown foundationdb:foundationdb $DATADIR
+    ln -s $DATADIR /var/lib/foundationdb/data
+fi
 
-service foundationdb start
+set -m
+/usr/lib/foundationdb/fdbmonitor --conffile $CONFFILE --lockfile /var/run/fdbmonitor.pid &
 
 CMD=status
 if [ "$NEWDB" = "yes" ]; then
@@ -47,3 +51,5 @@ if [ "$NEWDB" = "yes" ]; then
 fi
 
 fdbcli --exec "$CMD" --timeout 60
+
+fg
